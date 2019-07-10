829d7f6c000f205db82ba2dfc4fa1e2310c74323
Tiago Flavio Sola de Moraes <tiago.fsmoraes@hsl.org.br>
2019-07-10 14:56:12 -0300

    Android III: Aprimore sua app com Fragments, Google Maps e GPS - Aula 2 - Reaproveitando telas com Fragments - Atividade 14 (Finalizada);

--- app/src/main/java/alura/com/br/agenda/ProvasActivity.java
+++ app/src/main/java/alura/com/br/agenda/ProvasActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import alura.com.br.agenda.model.Prova;
+
 public class ProvasActivity extends AppCompatActivity {
 
     @Override
@@ -14,7 +16,37 @@ public class ProvasActivity extends AppCompatActivity {
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction tx = fragmentManager.beginTransaction();
-        //tx.replace(R.id.frame_principal, new ListaProvasFragment());
+
+        tx.replace(R.id.frame_principal, new ListaProvasFragment());
+        if (estaNoModoPaisagem()) {
+            tx.replace(R.id.frame_secundario, new DetalhesProvaFragment());
+        }
+
         tx.commit();
+    }
+
+    private boolean estaNoModoPaisagem() {
+        return getResources().getBoolean(R.bool.modoPaisagem);
+    }
+
+    public void selecionaProva(Prova prova) {
+        FragmentManager manager = getSupportFragmentManager();
+        if (!estaNoModoPaisagem()) {
+            FragmentTransaction tx = manager.beginTransaction();
+
+            DetalhesProvaFragment detalhesFragment = new DetalhesProvaFragment();
+            Bundle parametros = new Bundle();
+            parametros.putSerializable("prova", prova);
+            detalhesFragment.setArguments(parametros);
+
+            tx.replace(R.id.frame_principal, detalhesFragment);
+            tx.addToBackStack(null);  // adicionamos a transação na pilha
+
+            tx.commit();
+        } else {
+            DetalhesProvaFragment detalhesFragment =
+                    (DetalhesProvaFragment) manager.findFragmentById(R.id.frame_secundario);
+            detalhesFragment.populaCamposCom(prova);
+        }
     }
 }
